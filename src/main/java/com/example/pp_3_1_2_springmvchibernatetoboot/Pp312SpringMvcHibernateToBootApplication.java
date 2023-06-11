e82340c768f6b31aac5fbade9b3288416741d643
void-basis <teki-da@ya.ru>
2023-06-11 17:18:27 +0300

    Initial commit

---
+++ src/main/java/com/example/pp_3_1_2_springmvchibernatetoboot/Pp312SpringMvcHibernateToBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_1_2_springmvchibernatetoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp312SpringMvcHibernateToBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp312SpringMvcHibernateToBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pp_3_1_2_springmvchibernatetoboot/Pp312SpringMvcHibernateToBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_1_2_springmvchibernatetoboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312SpringMvcHibernateToBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
